27193498e73b6d7a672e31bb6bafec4a6a4e4d0e
Curtis Larson <quackware@gmail.com>
2023-02-11 13:58:58 -0500

    fix: Clip long song / artist name to prevent overflow, pad times with 0 if necessary and fix progress bar clipping, move favorite heart inside image incase there are long names

--- src/now-playing.ts
+++ src/now-playing.ts
@@ -17,6 +17,9 @@ setup({
   sheet,
 });
 
+const SVG_WIDTH = 308;
+const SVG_HEIGHT = 380;
+
 export interface NowPlayingProps {
   name: string;
   artistNames: string[];
@@ -34,7 +37,10 @@ function msToTime(s: number) {
   s = (s - secs) / 60;
   const mins = s % 60;
 
-  return mins + ":" + secs;
+  const minStr = mins < 10 ? `0${mins}` : mins;
+  const secStr = secs < 10 ? `0${secs}` : secs;
+
+  return `${minStr}:${secStr}`;
 }
 
 function renderProgressBar({ progress, duration }: NowPlayingProps) {
@@ -45,12 +51,12 @@ function renderProgressBar({ progress, duration }: NowPlayingProps) {
       ? Math.floor((progress / duration) * 100)
       : null;
     return html`
-    <div class="${tw`flex flex-row`}">
-      <div class="${tw`text-orange text-xs pr-1 mt-[5px]`}">${msToTime(progress)}</div>
-      <div class="${tw`w-full mt-2 bg-background-200 rounded-full h-2.5`}">
+    <div class="${tw`flex flex-row mt-[5px] items-center`}">
+      <div class="${tw`text-orange text-xs pr-1 flex-none`}">${msToTime(progress)}</div>
+      <div class="${tw`w-full grow bg-background-200 rounded-full h-2.5`}">
         <div class="${tw`bg-primary h-2.5 rounded-full`}" style="width:${progressPercent}%"></div>
       </div>
-      <div class="${tw`text-orange text-xs pl-1 mt-[5px]`}">${msToTime(duration)}</div>
+      <div class="${tw`text-orange text-xs pl-1 flex-none`}">${msToTime(duration)}</div>
     </div>
     `;
   }
@@ -59,7 +65,7 @@ function renderProgressBar({ progress, duration }: NowPlayingProps) {
 export function renderHeart(props: NowPlayingProps) {
   if (props.isFavorite) {
     return html`
-    <div class="${tw`absolute inset-b-10 right-4 w-8 text-primary`}">
+    <div class="${tw`absolute bottom-[90px] right-4 w-8 text-primary`}">
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
         <path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
       </svg>
@@ -69,18 +75,19 @@ export function renderHeart(props: NowPlayingProps) {
 
 export function renderNowPlayingCard(props: NowPlayingProps) {
   return html`
-    <div class="${tw`w-fit p-2 bg-background-500`}" xmlns="http://www.w3.org/1999/xhtml">
-      <img class="${tw`h-72 w-72`}" src="${props.imageUrl}"></img>
-      <h1 class="${tw`m-0 mt-1 text-base font-medium text-primary overflow-x-clip`}">${props.name}</h1>
-      <h2 class="${tw`m-0 text-sm font-normal text-secondary overflow-x-clip`}">${props.artistNames.join(", ")}</h2>
+    <div class="${tw`p-2 bg-background-500 flex flex-col max-w-fit whitespace-nowrap w-[300]`}" xmlns="http://www.w3.org/1999/xhtml">
+      <div class="flex-none"><img class="${tw`h-72 w-72`}" src="${props.imageUrl}"></img></div>
+      <div class="flex-none"><h1 class="${tw`m-0 mt-1 text-base font-medium text-primary overflow-hidden`}">${props.name}</h1></div>
+      <div class="flex-none"><h2 class="${tw`m-0 text-sm font-normal text-secondary overflow-hidden`}">${
+    props.artistNames.join(", ")
+  }</h2></div>
       ${renderHeart(props)}
-      ${renderProgressBar(props)}
+      <div class="flex-none w-[300]">
+        ${renderProgressBar(props)}
+      </div>
     </div>
   `;
 }
-
-const SVG_WIDTH = 308;
-const SVG_HEIGHT = 380;
 
 export function render(props: NowPlayingProps) {
   sheet.reset();
